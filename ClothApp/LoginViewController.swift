5c229827495cc63a7b8e280661007dc93a8073bc
ceribbo <giacomoceribelli@gmail.com>
2016-06-18 21:34:26 +0200

    login working correctly

--- ClothApp/LoginViewController.swift
+++ ClothApp/LoginViewController.swift
@@ -13,6 +13,8 @@ class LoginViewController: UIViewController {
     
     @IBOutlet weak var username: UITextField!
     @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var signupButton: UITextView!
+    @IBOutlet weak var forgotButton: UITextView!
     
     //pressed login button
     @IBAction func loginPressed(sender: UIButton) {
@@ -24,16 +26,45 @@ class LoginViewController: UIViewController {
             let alert = UIAlertController(title: "Attenzione!", message: message, preferredStyle: UIAlertControllerStyle.Alert)
             alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
             self.presentViewController(alert, animated: true, completion: nil)
+        }else{
+            //proceed with login
+            //get username capitalized or not
+            let username_check = PFUser.query()
+            username_check!.whereKey("lowercase", equalTo:(username.text?.lowercaseString)!)
+            username_check!.getFirstObjectInBackgroundWithBlock {
+                (object: PFObject?, error: NSError?) -> Void in
+                if error != nil || object == nil {
+                    parseErrorCheck(error!.code, uiViewController: self)
+                } else {
+                    //obtained correct username
+                    let usr:PFUser = (object as? PFUser)!
+                    PFUser.logInWithUsernameInBackground(usr.username!, password: self.password.text!)    {
+                        (user: PFUser?, error: NSError?) -> Void in
+                        if user != nil {
+                            // success
+                            //go back to splash screen
+                        } else {
+                            if (error?.code == 101) {
+                                parseErrorCheck(0, uiViewController: self)
+                            }else{
+                                parseErrorCheck(error!.code, uiViewController: self)
+                            }
+                        }
+                    }
+                }
+            }
         }
-        
-        //proceed with login
-        
         
     
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func viewDidAppear(animated: Bool) {
+        
+        if (signupButton.exclusiveTouch)    {
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let vc = storyboard.instantiateViewControllerWithIdentifier("SignupViewController") as UIViewController
+            presentViewController(vc, animated: true, completion: nil)
+        }
     }
     
     override func didReceiveMemoryWarning() {
---
+++ ClothApp/SignupViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SignupViewController.swift
+//  ClothApp
+//
+//  Created by Giacomo Ceribelli on 18/06/16.
+//  Copyright © 2016 Giacomo Ceribelli. All rights reserved.
+//
+
+import Foundation
+import Parse
+
+class SignupViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
